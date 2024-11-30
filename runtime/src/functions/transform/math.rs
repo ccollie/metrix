b07bc46a783388f206e8b23e9f07773904ee36da
ccollie <gboyah@gmail.com>
2024-11-30 00:19:01 -0500

    Use `total_cmp` instead of 'partial_cmp'

--- runtime/src/functions/transform/math.rs
+++ runtime/src/functions/transform/math.rs
@@ -56,14 +56,10 @@ pub(crate) fn sgn(tfa: &mut TransformFuncArg) -> RuntimeResult<Vec<Timeseries>> 
     let tf = |values: &mut [f64]| {
         let mut zero = 0.0f64;
         for v in values {
-            if let Some(value) = v.partial_cmp(&&mut zero) {
-                *v = match value {
-                    std::cmp::Ordering::Less => -1.0f64,
-                    std::cmp::Ordering::Equal => 0.0f64,
-                    std::cmp::Ordering::Greater => 1.0f64,
-                }
-            } else {
-                *v = f64::NAN;
+            match v.total_cmp(&&mut zero) {
+                std::cmp::Ordering::Less => *v = -1.0f64,
+                std::cmp::Ordering::Equal => *v = 0.0f64,
+                std::cmp::Ordering::Greater => *v = 1.0f64,
             }
         }
     };
