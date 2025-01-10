5208f9195470043eb4c3d2174fd43660e92dc970
ccollie <gboyah@gmail.com>
2025-01-10 11:56:01 -0500

    Add license to unit_vec

--- common/src/types/unit_vec.rs
+++ common/src/types/unit_vec.rs
@@ -1,3 +1,26 @@
+// Copyright (c) 2020 Ritchie Vink
+// Some portions Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+// Original source Polars
+// https://github.com/pola-rs/polars/blob/main/crates/polars-utils/src/idx_vec.rs
+
 use serde::{Serialize, Deserialize, Serializer, Deserializer};
 use core::mem::align_of;
 use core::mem::size_of;
@@ -6,9 +29,6 @@ use std::fmt::{Debug, Formatter};
 use std::num::NonZeroUsize;
 use std::ops::Deref;
 use serde::de::{SeqAccess, Visitor};
-// Original source Polars
-// https://github.com/pola-rs/polars/blob/main/crates/polars-utils/src/idx_vec.rs
-
 pub type IdxVec = UnitVec<usize>;
 
 /// A type logically equivalent to `Vec<T>`, but which does not do a
